8b1c1cfe7a79f6fa8080bfc7244f66d6fc39d638
Benjamin Izrailov <56977679+MediumBen@users.noreply.github.com>
2021-09-21 17:08:49 -0400

    Created spring project

---
+++ soundsource-backend/src/main/java/soundsource/springframework/soundsourcebackend/SoundsourceBackendApplication.java
@@ -0,0 +1,13 @@
+package soundsource.springframework.soundsourcebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundsourceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundsourceBackendApplication.class, args);
+	}
+
+}
---
+++ soundsource-backend/src/test/java/soundsource/springframework/soundsourcebackend/SoundsourceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package soundsource.springframework.soundsourcebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundsourceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
