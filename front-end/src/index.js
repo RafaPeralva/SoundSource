2c0ff4812b15d627dc43e602253cf9491008501f
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-10-06 22:20:40 -0400

    fix some missing files/config to frontend

--- front-end/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-
-import React from 'react';
-
-const App = () => {
- return <h1> Here would go some SoundSource code </h1>;
- }
-ReactDOM.render(<App />, document.getElementById('app'));
