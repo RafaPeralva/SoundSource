04bf02a4012a9d74346f41597db50df0d8e02dae
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-10-10 08:25:48 -0400

    homepage and bootstrap setting

--- front-end/soundsource/src/components/Homepage/Homepage.js
+++ front-end/soundsource/src/components/Homepage/Homepage.js
@@ -1,9 +1,37 @@
 import React from "react";
 import { Container, Row, Col, RowProps, ColProps } from "react-bootstrap";
+
 export const Homepage = () => {
   return (
     <div className="homepage">
-      <p>heres some homepage content</p>
+      <div className="homepage-body">
+        <Container fluid>
+          {/* for now cols and rows are manually generated
+            once backend is set up we will do it with a map function */}
+          <Row>
+            <Col className="text-center">
+              <h3>Suggested</h3>
+            </Col>
+            <Col className="text-center">
+              <h3>Playlists</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center">
+              here goes the suggested songs ...
+              <Row>
+                <Col className="text-center">with new rows </Col>
+              </Row>
+            </Col>
+            <Col className="text-center">
+              here goes the suggested Playlists ...
+              <Row>
+                <Col className="text-center">with new rows</Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
