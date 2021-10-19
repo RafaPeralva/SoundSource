61276d1b232c0e130b1a93285e0678a0cbd60c73
Shabab Khan <shababkhanSWE@gmail.com>
2021-10-19 16:45:03 -0400

    MainPlaylist
    MainPlaylist hardcoded

--- front-end/soundsource/src/components/Homepage/Homepage.js
+++ front-end/soundsource/src/components/Homepage/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Homepage.css";
 import { Container, Row, Col, RowProps, ColProps } from "react-bootstrap";
 
 export const Homepage = () => {
@@ -27,9 +28,18 @@ export const Homepage = () => {
               </Row>
             </Col>
             <Col className="text-center">
-              here are the main Playlist ...
+              <ul className="playlist">
+                <li>Bad Romance</li>
+                <li className="artist">Lady Gaga</li>
+                <li>Location</li>
+                <li className="artist">Khalid</li>
+                <li>Off The Grid</li>
+                <li className="artist">Kanye West</li>
+                <li>Way 2 Sexy</li>
+                <li className="artist">Drake, Future</li>
+              </ul>
               <Row>
-                <Col className="text-center">with new rows </Col>
+                <Col className="text-center"> </Col>
               </Row>
             </Col>
             <Col className="text-center">
