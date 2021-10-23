98d5f60a4ba0e845d77acf6cd9302711bd17c15e
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-10-23 17:09:38 -0400

    store input

--- front-end/soundsource/src/components/SearchBar/SearchBar.js
+++ front-end/soundsource/src/components/SearchBar/SearchBar.js
@@ -2,25 +2,40 @@ import React from "react";
 import { Button } from "../Button/Button";
 import "./SearchBar.css";
 
-export const SearchBar = () => {
-  return (
-    <div className="searchWrapper">
-      <form action="/" method="get">
-        <div className="searchBar">
-          <input
-            type="text"
-            id="navbar-search"
-            placeholder="Search..."
-            name="search"
-            className="searchInput"
-          />
-          <Button className="searchBtn" type="submit">
-            Search
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
-};
+export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: "",
+    };
+  }
+  handleChange(e) {
+    this.setState({ searchValue: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="searchWrapper">
+        <form action="/" method="get">
+          <div className="searchBar">
+            <input
+              type="text"
+              id="navbar-search"
+              placeholder="Search..."
+              name="search"
+              className="searchInput"
+              onChange={this.handleChange.bind(this)}
+            />
+            {console.log(this.state.searchValue)}
+
+            <Button className="searchBtn" type="submit">
+              Search
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
