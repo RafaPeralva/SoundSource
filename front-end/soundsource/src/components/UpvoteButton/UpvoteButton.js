466ac0c13fd30d1411714641b7799982128bb534
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-11-21 18:30:08 -0500

    upvote

--- front-end/soundsource/src/components/UpvoteButton/UpvoteButton.js
+++ front-end/soundsource/src/components/UpvoteButton/UpvoteButton.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./UpvoteButton.css";
 
-export const UpvoteButton = ({ onClick }) => {
+export const UpvoteButton = () => {
+    const [disabled, setDisabled = useState(false);
+  console.log(disabled);
+  handleOnCLick = () => {
+    this.setState({ disabled: false });
+    console.log(disabled);
+  };
   return (
+    // this.state = {
+    //     disabled: false
+    //  }
     <button>
       <img
         src="/images/upvote.png"
         type="button"
         width="20"
-        className="btn"
+        className={this.handleOnCLick}
       ></img>
     </button>
     // <button
