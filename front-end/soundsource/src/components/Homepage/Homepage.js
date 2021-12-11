4758df3783d1a9395e089f9c90f5952fd373c246
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-12-10 22:19:17 -0500

    playlist func

--- front-end/soundsource/src/components/Homepage/Homepage.js
+++ front-end/soundsource/src/components/Homepage/Homepage.js
@@ -6,6 +6,7 @@ import { Suggested } from "../Suggested/Suggested";
 import axios from "axios";
 import PlaylistDisplay from "../Playlist/PlaylistDisplay";
 import Playlist from "../Playlist/Playlist";
+
 export const Homepage = () => {
   function exportSuggested() {
     axios.get("http://localhost:8080/suggested").then((response) => {
@@ -37,6 +38,38 @@ export const Homepage = () => {
       }
     });
   }
+  
+  export const PlaylistsPage = () => {
+    function exportPlaylist() {
+      axios.get("http://localhost:8080/playlist").then((response) => {
+        if (response.data[0] != null) {
+          let count = 0;
+          let songString = [];
+          for (var i in response.data) {
+            songString[count] = "spotify:track:" + response.data[count].id;
+            count++;
+          }
+          let linkToAPI =
+            "http://localhost:8080/api/exportPlaylist?q=" + songString;
+          axios.get(linkToAPI).then((response) => {});
+        }
+      });
+    }
+  
+    function playPlaylist() {
+      axios.get("http://localhost:8080/playlist").then((response) => {
+        if (response.data[0] != null) {
+          let count = 0;
+          let songString = [];
+          for (var i in response.data) {
+            songString[count] = "spotify:track:" + response.data[count].id;
+            count++;
+          }
+          let linkToAPI = "http://localhost:8080/api/play?q=" + songString;
+          axios.get(linkToAPI).then((response) => {});
+        }
+      });
+    }
   return (
     <div className="homepage">
       <div className="homepage-body">
