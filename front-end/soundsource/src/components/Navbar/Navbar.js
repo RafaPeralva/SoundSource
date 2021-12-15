c7a525537315116fc70748de5b9bc672d4fd4dab
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-12-14 19:24:39 -0500

    navbar component

--- front-end/soundsource/src/components/Navbar/Navbar.js
+++ front-end/soundsource/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { MenuItems } from "./MenuItems";
 import { Button } from "../Button/Button";
 import "./Navbar.css";
@@ -11,44 +11,42 @@ const getSpotifyUserLogin = () => {
     });
 };
 
-export class Navbar extends Component {
-  state = { clicked: false };
+export const Navbar = () => {
+  // state = { clicked: false };
 
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
   };
 
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <img src="/images/wave.png" alt="note" className="wave" />
-        <a href="/" className="navbar-logo">
-          SoundSource
-        </a>
+  return (
+    <nav className="NavbarItems">
+      <img src="/images/wave.png" alt="note" className="wave" />
+      <a href="/" className="navbar-logo">
+        SoundSource
+      </a>
 
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="navLinkDiv"></div>
-        <div className="navButton">
-          <Button className="loginbtn" onClick={getSpotifyUserLogin}>
-            Login
-          </Button>
-        </div>
-      </nav>
-    );
-  }
-}
+      <div className="menu-icon" onClick={this.handleClick}>
+        <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="navLinkDiv"></div>
+      <div className="navButton">
+        <Button className="loginbtn" onClick={getSpotifyUserLogin}>
+          Login
+        </Button>
+      </div>
+    </nav>
+  );
+};
